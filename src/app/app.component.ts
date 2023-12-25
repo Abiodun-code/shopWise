0cf9bcab49874cb0acbee4ce4a40cf3b02fd93b9
unknown <Abiodun-code.com>
2023-12-24 21:51:26 -0800

    adding header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shopWise';
+
+  navScroll:boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(){
+    if (window.scrollY <= 100) {
+      this.navScroll = true;
+    } else {
+      this.navScroll = false;
+    }
+  }
 }
